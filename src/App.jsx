7e5bfbc8bc68c1a9b8e71777fa7cfbae9c9fd0d4
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2024-10-07 20:31:21 +0200

    props short syntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,12 +22,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -40,14 +40,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts!</h2>
           <ul>
-            <CoreConcept 
-            title="Components" 
-            description="The core UI building block"
-            image={componentsImg}
-            />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
@@ -56,3 +52,4 @@ function App() {
 }
 
 export default App;
+
