177e3711f5529e86e246d2d5f8f673fac6a0edc7
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2024-10-11 18:22:57 +0200

    hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState} from "react" 
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
 import Header from "../src/components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleClick(selectedButton) {
-    console.log(selectedButton);
-    
-}
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
 
   return (
     <div>
@@ -26,12 +29,22 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={() => handleClick('component')}>Components</TabButton>
-            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
-            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
-            <TabButton onClick={() => handleClick('state')}>State</TabButton>
+            <TabButton onClick={() => handleClick("component")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          <div id='tab-content'>
+            <h3>{ EXAMPLES[selectedTopic].title}</h3>
+            <p>{ EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              { EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
